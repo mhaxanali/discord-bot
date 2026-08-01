4310d5e039295c757d05be3f964b9bff30382559
Muhammad Hasan <mhasanali2010@gmail.com>
2026-08-01 12:53:05 +0500

    Add Events cog

---
+++ bot/cogs/events.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+from bot.logger import Logger
+
+log = Logger()
+
+class Events(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        log.command(
+            user=str(ctx.author),
+            command_name=ctx.command.qualified_name,
+            guild=ctx.guild.name
+        )
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Events(bot))
