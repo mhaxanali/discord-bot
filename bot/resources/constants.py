61670bfed8b3cd3154f56772afb43726799659e0
Muhammad Hasan <mhasanali2010@gmail.com>
2026-08-01 12:31:49 +0500

    Setup constants.py

---
+++ bot/resources/constants.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found in environment, check your .env file")
+
+
+with open("config.json", "r") as f:
+    data = json.load(f)
+
+DB_PATH = data["db_path"]
