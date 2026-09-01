e671c1280573908043f060d29245b14f6dbb919a
mhaxanali <mhasanali2010@gmail.com>
2026-09-01 12:13:46 +0500

    Add config cog structure

---
+++ bot/cogs/config.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from bot.logger import Logger
+
+from discord import Embed, Colour
+
+log = Logger()
+
+class Config(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.group(name="config")
+    async def config(self, ctx):
+        embed = Embed(
+            title="Config Commands",
+            description="This cog contains commands to edit the server-wide config",
+            colour=Colour.blue()
+        )
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Config(bot))
