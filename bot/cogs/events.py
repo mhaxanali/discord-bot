7012cdda0714754e5852443577c96341a083ce45
mhaxanali <mhasanali2010@gmail.com>
2026-09-02 15:15:33 +0500

    add listeners for on_guild_join and on_guild_remove

--- bot/cogs/events.py
+++ bot/cogs/events.py
@@ -1,4 +1,5 @@
 # default events cog, always enabled
+import discord
 from discord.ext import commands
 from bot.logger import Logger
 
@@ -8,7 +9,6 @@ class Events(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
         log.command(
@@ -16,6 +16,15 @@ class Events(commands.Cog):
             command_name=ctx.command.qualified_name,
             guild=ctx.guild.name
         )
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: discord.Guild):
+        await self.bot.connector.create_guild_config(guild.id)
+        log.info(f"Joined guild {guild.name} ({guild.id}), config initialized")
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild: discord.Guild):
+        log.warning(f"Removed from guild {guild.name} ({guild.id})")
 
 
 async def setup(bot: commands.Bot):
