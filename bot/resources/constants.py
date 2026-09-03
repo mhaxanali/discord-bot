3733929ab3606a5a050f5dd3164c598032c7aed6
mhaxanali <mhasanali2010@gmail.com>
2026-09-03 18:37:40 +0500

    Add BOT_OWNER_ID environment variable

--- bot/resources/constants.py
+++ bot/resources/constants.py
@@ -6,9 +6,11 @@ import json
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_OWNER_ID = os.getenv("BOT_OWNER_ID")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not found in environment, check your .env file")
-
+if not BOT_OWNER_ID:
+    raise ValueError("BOT_OWNER_ID not found in environment, check your .env file")
 
 with open("config.json", "r") as f:
     data = json.load(f)
